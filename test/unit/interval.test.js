5bb249a7610451aeb9fcc97ed63dc4a566e76b41
Marwan <mwgg@sfr.fr>
2019-09-26 17:18:45 +0200

    correction test

--- test/unit/interval.test.js
+++ test/unit/interval.test.js
@@ -1,6 +1,5 @@
-const Util = require('./src/interval');
-
-test('Test factoriel de 2 => 2', () => {
+const Interval = require('../../src/interval');
+test('Test Interval Overlaps', () => {
     let a = new Interval(0, 5);
     let b = new Interval(2, 7); 
     expect(a.overlaps(b)).toBe(true);
