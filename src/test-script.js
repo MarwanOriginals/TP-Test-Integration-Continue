1cd643b705fd16a1f0b97753295f800a2d9f4f2f
MARWAN <mwgg@sfr.fr>
2019-10-10 04:05:27 +0200

    Las function

--- src/test-script.js
+++ src/test-script.js
@@ -3,13 +3,14 @@ const db = require('./db')
 
 const repository = new BookRepository(db);
 
-
+/*
 repository.save({
     'id' : 1,
     "name" :"test",
     'price' :6.1,
     "added_at" : '2019-01-01'
 });
-
-console.log(repository.getTotalPrice());
-console.log(repository.getTotalCount());
+*/
+//console.log(repository.getTotalPrice());
+//console.log(repository.getBookByName("issou"));
+console.log(repository.getCountBookAddedByMont("issou"));
